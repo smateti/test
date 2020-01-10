c987f63375aaf30588d692a2dfc622fc725bb9ba
shyam <smateti@gmail.com>
2020-01-09 22:02:01 -0500

    Update Zot.groovy

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -2,6 +2,22 @@ package org.foo
 
 def checkOutFrom(repo) {
   echo "$repo"
+   def url = new URL("https://github.com/smateti/hello/blob/master/README.md")
+    HttpURLConnection connection = url.openConnection()
+
+    connection.setRequestMethod("GET")
+    connection.setDoInput(true)
+
+    try {
+        connection.connect()
+        new InputStreamReader(connection.getInputStream(), "UTF-8")
+        return true
+    } catch (FileNotFoundException e1) {
+        echo "File not yet available: ${url.toString()}"
+        return false
+    } finally {
+        connection.disconnect()
+    }
 }
 
 return this
